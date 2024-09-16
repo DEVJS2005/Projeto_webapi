4c3da0dcc23a786dac158697abd005f49c2cf641
DEVJS2005 <jvl14533@outlook.com>
2024-09-15 23:47:04 -0300

    Web api na versão 01, crianda apenas um teste

---
+++ projeto_webapi/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace proejto_webapi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController : Controller
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToLongTimeString(),
+            };
+            return Ok(obj);
+        }
+    }
+}
